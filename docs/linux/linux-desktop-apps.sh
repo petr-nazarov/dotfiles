ba767fbf864c2ed2025d3e076ff1332f9dc81663
Petr Nazarov <mrzueck@gmail.com>
2024-01-18 12:40:17 +0200

    Update dotfiles

--- docs/linux/linux-desktop-apps.sh
+++ docs/linux/linux-desktop-apps.sh
@@ -1,1 +1,1 @@
-yay -S --noconfirm brave-bin kitty vlc flameshot slack-desktop postman-bin studio-3t zoom
+yay -S --noconfirm brave-bin kitty vlc flameshot slack-desktop postman-bin studio-3t zoom okular nemo
