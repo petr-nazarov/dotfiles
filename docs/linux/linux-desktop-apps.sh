9b703b5010090f0c53b648baabda027b1a7677c7
Nazarov Petr <mrzueck@gmail.com>
2024-02-07 11:11:16 +0200

    Update linux-desktop-apps.sh

--- docs/linux/linux-desktop-apps.sh
+++ docs/linux/linux-desktop-apps.sh
@@ -1,1 +1,1 @@
-yay -S --noconfirm brave-bin kitty vlc flameshot slack-desktop postman-bin studio-3t zoom okular nemo qbittorrent
+yay -S --noconfirm brave-bin kitty vlc flameshot slack-desktop postman-bin studio-3t okular nemo qbittorrent
