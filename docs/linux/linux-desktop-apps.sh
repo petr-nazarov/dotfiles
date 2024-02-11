352852b488898d9fc03a02f7f0edd62400d30516
Petr Nazarov <mrzueck@gmail.com>
2024-02-11 11:40:28 +0200

    aaa

--- docs/linux/linux-desktop-apps.sh
+++ docs/linux/linux-desktop-apps.sh
@@ -1,1 +1,1 @@
-yay -S --noconfirm brave-bin kitty vlc flameshot slack-desktop postman-bin studio-3t okular nemo qbittorrent
+yay -S --noconfirm brave-bin kitty vlc slack-desktop-wayland postman-bin studio-3t okular nemo qbittorrent
