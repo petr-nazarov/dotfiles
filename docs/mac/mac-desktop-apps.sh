9b72d0ef86a907e5542fb046546d09e1c59607eb
Nazarov Petr <petr.tzur.nazarov@gmail.com>
2024-06-17 14:16:39 +0300

    Update mac-desktop-apps.sh

--- docs/mac/mac-desktop-apps.sh
+++ docs/mac/mac-desktop-apps.sh
@@ -23,15 +23,8 @@ skhd --start-service
 # create a new file for writing - visudo uses the vim editor by default.
 # go read about this if you have no idea what is going on.
 
-sudo visudo -f /private/etc/sudoers.d/yabai
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
 
-# input the line below into the file you are editing.
-#  replace <yabai> with the path to the yabai binary (output of: which yabai).
-#  replace <user> with your username (output of: whoami). 
-#  replace <hash> with the sha256 hash of the yabai binary (output of: shasum -a 256 $(which yabai)).
-#   this hash must be updated manually after running brew upgrade.
-
-<user> ALL=(root) NOPASSWD: sha256:<hash> <yabai> --load-sa
 
 curl -sS https://starship.rs/install.sh | sh
 
