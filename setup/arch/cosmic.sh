8caeb7990c4e68ffdbabc3e757403beb0f2ccd40
Petr Nazarov <petr.tzur.nazarov@gmail.com>
2025-05-16 17:10:47 +0300

    WIP

---
+++ setup/arch/cosmic.sh
@@ -0,0 +1,5 @@
+yay -Suv cosmic-session
+sudo systemctl enable cosmic-greeter.service
+sudo systemctl start  cosmic-greeter.service
+sudo systemctl enable --now NetworkManager
+
--- setup/arch/hyperland.sh
+++ setup/arch/hyperland.sh
@@ -1,4 +1,6 @@
-yay -S --noconfirm swww polkit-kde-agent sdbus-cpp-git hyprland swaync qt6-wayland rofi-lbonn-wayland-git cliphist sww grimblast iniparser waybar 
+yay -S --noconfirm swww polkit-kde-agent sdbus-cpp-git hyprland swaync qt6-wayland rofi-lbonn-wayland-git cliphist sww grimblast iniparser waybar  brightnessctl hypridle hyprlock  network-manager-applet libsForQt5.plasma-nm blueman pipewire wireplumber pavucontrol greetd-regreet greetd-tuigreet 
+
+ 
 
 #yay -S --noconfirm pipewire wireplumber xdg-desktop-portal-hyprland-wlr grim slurp 
 ## yes install wlr, the hyperland configuration does not work
