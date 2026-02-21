adcf219dc11aa7c48b31a8a313719e5de41312dd
Petr Nazarov <petr.tzur.nazarov@gmail.com>
2026-02-21 19:09:48 +0200

    WIP

--- _common/.local/bin/sunshine-stream-end.sh
+++ _common/.local/bin/sunshine-stream-end.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1. Re-enable physical monitor
-hyprctl keyword monitor "DP-2, 3440x1440@144, 0x0, 1"
+hyprctl keyword monitor "DP-1, 3440x1440@144, 0x0, 1"
 
 # 2. Delete the virtual display to save resources
 hyprctl monitors -j | jq -r '.[] | select(.name | startswith("HEADLESS")) | .name' | xargs -I {} hyprctl output remove {}
--- _common/.local/bin/sunshine-stream-start.sh
+++ _common/.local/bin/sunshine-stream-start.sh
@@ -9,11 +9,11 @@ systemctl --user stop hypridle
 # 2. Set resolution (adjust to your client's screen)
 hyprctl keyword monitor "${MONITOR_NAME}, 3840x2160@60, 0x0, 1"
 
-sleep 1
-hyprctl dispatch moveworkspacetomonitor 8 ${MONITOR_NAME}
-hyprctl dispatch movetoworkspacesilent 8,title:^Lutris$
-hyprctl dispatch workspace 8
-hyprctl keyword cursor:no_hardware_cursors true
+# sleep 1
+# hyprctl dispatch moveworkspacetomonitor 8 ${MONITOR_NAME}
+# hyprctl dispatch movetoworkspacesilent 8,title:^Lutris$
+# hyprctl dispatch workspace 8
+# hyprctl keyword cursor:no_hardware_cursors true
 
 # 3. Disable your physical monitor (Change 'DP-1' to your real monitor name)
-hyprctl keyword monitor "DP-2, disable"
+# hyprctl keyword monitor "DP-1, disable"
